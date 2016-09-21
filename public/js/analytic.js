25ba6923babb362360b733f8ddc1e38cf93671b4
Tittaya <callmefons@gmail.com>
2016-09-21 22:30:59 +0700

    change clicky

--- public/js/analytic.js
+++ public/js/analytic.js
@@ -1,4 +1,21 @@
-function ClickyLog(name) {
-    console.log(name);
-    clicky.log(name.toString(),name);
+// function ClickyLog(name) {
+//     console.log(name);
+//     clicky.log(name.toString(),name.toString());
+// }
+
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(100980200);
+(function() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//static.getclicky.com/js';
+    ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+
+function ClickyLog(app) {
+    console.log("click", app);
+    clicky.log(app, app);
 }
+
+
