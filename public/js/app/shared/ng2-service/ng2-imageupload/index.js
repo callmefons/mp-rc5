6b7a51189c59aef0b653460129c0f010f0c27d0b
Nattaya Mairittha <nattafahh@gmail.com>
2016-09-30 16:38:55 +0700

    Updated

--- public/js/app/shared/ng2-service/ng2-imageupload/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./src/image-upload'));
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNoYXJlZC9uZzItc2VydmljZS9uZzItaW1hZ2V1cGxvYWQvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUNBLGlCQUFjLG9CQUFvQixDQUFDLEVBQUEiLCJmaWxlIjoic2hhcmVkL25nMi1zZXJ2aWNlL25nMi1pbWFnZXVwbG9hZC9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gJy4vc3JjL2ludGVyZmFjZXMnO1xuZXhwb3J0ICogZnJvbSAnLi9zcmMvaW1hZ2UtdXBsb2FkJztcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
--- public/js/app/shared/ng2-service/ng2-imageupload/src/interfaces.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzaGFyZWQvbmcyLXNlcnZpY2UvbmcyLWltYWdldXBsb2FkL3NyYy9pbnRlcmZhY2VzLmpzIiwic291cmNlc0NvbnRlbnQiOltdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
